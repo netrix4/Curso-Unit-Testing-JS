1a7129ecb5543a9080066da3ee02946e0c5d329f
Mario <netrix4@gmail.com>
2025-04-22 16:35:40 -0700

    First Challenge

--- code/basic-testing-frontend/src/util/numbers.test.js
+++ code/basic-testing-frontend/src/util/numbers.test.js
@@ -1,5 +1,44 @@
 import { describe, expect, it } from "vitest";
 import { cleanNumbers } from "./numbers";
+import { transformToNumber } from "./numbers";
+
+describe("About numbers testin", () => {
+  it("should return a valid number even if a spaces-problematic string is provided", () => {
+    // AAA Asert, Action,
+
+    // Arrange
+    const spaceProblematicString = "   1997 ";
+
+    // Action
+    const bornYear = transformToNumber(spaceProblematicString);
+
+    // Assert
+    expect(bornYear).toBe(1997);
+  });
+  it("should throw an error if the provided value is non a valid number string", () => {
+    //Arrange
+    const alphaNumericValue = "1283756123A";
+    // Act
+    const returnedError = transformToNumber(alphaNumericValue);
+
+    //Assert
+    expect(returnedError).toBeInstanceOf(Error);
+  });
+  it("should accept negative and positive numbers", () => {
+    // Arrange
+    const positiveNumber = "299";
+    const negativeNumber = "-199";
+    // Act
+    const positiveResponse = transformToNumber(positiveNumber);
+    const negativeResponse = transformToNumber(negativeNumber);
+
+    console.log("Responses: ", positiveNumber, negativeNumber);
+
+    // Assertion
+    expect(positiveResponse).toBeGreaterThan(0);
+    expect(negativeResponse).toBeLessThan(0);
+  });
+});
 
 describe("About cleanNumbers function", () => {
   it("should return an array of number values if an arrary of string number values is provided", 
