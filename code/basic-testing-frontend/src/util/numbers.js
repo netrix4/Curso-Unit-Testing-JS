d77b64e0c54c7dd04192c89498ebe62dcf9e7c27
Mario <netrix4@gmail.com>
2025-04-22 17:08:39 -0700

    First Challenge wrapped in a function

--- code/basic-testing-frontend/src/util/numbers.js
+++ code/basic-testing-frontend/src/util/numbers.js
@@ -1,16 +1,23 @@
-import {validateStringNotEmpty, validateNumber} from './validation'
+import { validateStringNotEmpty, validateNumber } from "./validation";
 
 export function transformToNumber(value) {
+  const letterRegex = /[a-zA-Z]/g;
+  if (typeof value == "string") {
+    value.trim();
+    if (letterRegex.test(value)) {
+      throw new Error("The provided value has one or more letters");
+    }
+  }
   return +value;
 }
 
 export function cleanNumbers(numberValues) {
   const numbers = [];
   for (const numberInput of numberValues) {
-    validateStringNotEmpty(numberInput)
-    const number = transformToNumber(numberInput)
-    validateNumber(number)
-    numbers.push(number)
+    validateStringNotEmpty(numberInput);
+    const number = transformToNumber(numberInput);
+    validateNumber(number);
+    numbers.push(number);
   }
   return numbers;
 }
--- code/basic-testing-frontend/src/util/numbers.test.js
+++ code/basic-testing-frontend/src/util/numbers.test.js
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, test } from "vitest";
 import { cleanNumbers } from "./numbers";
 import { transformToNumber } from "./numbers";
 
@@ -15,14 +15,16 @@ describe("About numbers testin", () => {
     // Assert
     expect(bornYear).toBe(1997);
   });
-  it("should throw an error if the provided value is non a valid number string", () => {
+  test("should throw an error if the provided value is non a valid number string", () => {
     //Arrange
     const alphaNumericValue = "1283756123A";
     // Act
-    const returnedError = transformToNumber(alphaNumericValue);
+    const tryError = () => {
+      transformToNumber(alphaNumericValue);
+    };
 
     //Assert
-    expect(returnedError).toBeInstanceOf(Error);
+    expect.soft(tryError).toThrowError(/letters/);
   });
   it("should accept negative and positive numbers", () => {
     // Arrange
@@ -32,8 +34,6 @@ describe("About numbers testin", () => {
     const positiveResponse = transformToNumber(positiveNumber);
     const negativeResponse = transformToNumber(negativeNumber);
 
-    console.log("Responses: ", positiveNumber, negativeNumber);
-
     // Assertion
     expect(positiveResponse).toBeGreaterThan(0);
     expect(negativeResponse).toBeLessThan(0);
@@ -41,16 +41,17 @@ describe("About numbers testin", () => {
 });
 
 describe("About cleanNumbers function", () => {
-  it("should return an array of number values if an arrary of string number values is provided", 
-    () => {
-        const numberValues = ['1','2']
-        const cleanedNumbers = cleanNumbers(numberValues)
-        expect(cleanedNumbers[0]).toBeTypeOf('number')
-    });
-    it('should throw an error if an array with at least one empty string is provided', ()=>{
-        const numberValues = [33,'',10]
-        const tempFunct = ()=> {cleanNumbers(numberValues)}
+  it("should return an array of number values if an arrary of string number values is provided", () => {
+    const numberValues = ["1", "2"];
+    const cleanedNumbers = cleanNumbers(numberValues);
+    expect(cleanedNumbers[0]).toBeTypeOf("number");
+  });
+  it("should throw an error if an array with at least one empty string is provided", () => {
+    const numberValues = [33, "", 10];
+    const tempFunct = () => {
+      cleanNumbers(numberValues);
+    };
 
-        expect(tempFunct).toThrow()
-    })
+    expect(tempFunct).toThrow();
+  });
 });
